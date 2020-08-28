232ceff415bfa107e9497286351135aff1bbc45c
Tobias Winchen <tobias.winchen@rwth-aachen.de>
2020-08-28 12:06:18 +0000

    Updated ip utils

--- mpikat/utils/ip_utils.py
+++ mpikat/utils/ip_utils.py
@@ -13,6 +13,7 @@ def split_ipstring(ips):
     Split an string of form iprange:port, e.g. 123.0.0.4+7:1234 into baseip,
     total number of ips and port. Raise an exception on invalid ip, port or number.
     """
+    log.debug("Checing string: {}".format(ips))
     try:
         if ":" not in ips:
             R = ips
@@ -34,9 +35,9 @@ def split_ipstring(ips):
     return ip, N, port
 
 
-def is_validat_multicast_range(ips):
+def is_validat_multicast_range(ip, N, port):
     """
-    Validates a multicast range of format 225.0.0.4+7:1234.
+    Validates a multicast ranges of format 225.0.0.4, 7, 1234.
         - The base ip has to be a valid multicast address.
         - The address range has to be a valid power of two.
         - The address has to be a multiple of the blocksize. A block of 8 can
@@ -44,25 +45,37 @@ def is_validat_multicast_range(ips):
           This is a limitation we inherited from the casperfpga skarab firmware(s).
     The function will return True if the string specifies a valid range and false otherwise. IF the string cannot be interpreted an error is raised.
     """
-
-    log.debug("Checing network string: {}".format(ips))
-    ip, N, port = split_ipstring(ips)
     if (N & (N - 1)) != 0:
         log.warning("{} is not a power of 2".format(N))
         return False
-    network = ipaddress.ip_network('{}/{}'.format(ip, 32-(N).bit_length() - 1), strict=False)
-    log.debug(" Validating network {}".format(network))
+    try:
+        network = ipaddress.ip_network('{}/{}'.format(ip, 32-(N.bit_length() - 1), strict=False))
+    except ValueError as V:
+        log.warning("{} does not start a valid range of size {} ({})".format(ip, 2**(N.bit_length()-1), N))
+        return False
 
+    log.debug(" Validating network {}".format(network))
     if not network.is_multicast:
         log.warning("{} is not a multicsat address".format(ip))
         return False
 
     if not network[0] == ipaddress.ip_address(ip):
-        log.warning("{} does not start a valid range of size {} ({}) - please use {}".format(ip, 2**N.bit_length(), N, network))
+        log.warning("{} does not start a valid range of size {} ({}) - please use {}".format(ip, 2**(N.bit_length()-1), N, network))
         return False
     return True
 
 
+def ipstring_from_list(ipstrings):
+    """
+    Creates a string of format a.b.c.d+x from a list of ips.
+    """
+    ips = [ipaddress.IPv4Address(ip) for ip in ipstrings]
+    ips.sort()
+
+    if ips[0] + len(ips) - 1 != ips[-1]:
+        raise RuntimeError("IP list not continous")
+
+    return "{}+{}".format(ips[0].compressed, len(ips) - 1)
 
 
 
